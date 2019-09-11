a543c0a26e676dad3d0f8dd5aabb3168d1b70c35
codyowl <codyowl@gmail.com>
2019-09-11 19:10:25 +0530

    apple_and_orange.py added :rocket:

---
+++ apple_and_orange.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_distance = [apple for apple in apples if s <= a+apple <=t]
+    orange_distance = [orange for orange in oranges if s<= b+orange <=t]
+    print (apple_distance)
+    print (orange_distance)
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
